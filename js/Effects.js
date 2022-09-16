748c5a2e1d5beace55f4a77e23777d99b7a3946c
Guilherme Cintra <gui7lk@gmail.com>
2022-09-16 14:15:17 -0300

    Quase todas funcionalidades adicionadas, falta apenas o número de curtidas

--- js/Effects.js
+++ js/Effects.js
@@ -2,3 +2,19 @@ function Like(x,y){
     x.classList.toggle('Off');
     y.classList.toggle('Off');
 }
+
+function alterUser(){
+    let userName = document.getElementById('userName');
+    let userImg = document.getElementById('userImg');
+
+    const newUserName = prompt('Digite o novo nome de usuário');
+    const newUserImg = prompt('Digite a url da nova imagem de perfil');
+
+    userName.textContent = newUserName;
+    userImg.innerHTML = `<img src="${newUserImg}" alt=""/>`;
+}
+
+function savePost(nonSaved, saved){
+    nonSaved.classList.toggle('Off');
+    saved.classList.toggle('Off');
+}
